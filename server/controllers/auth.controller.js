7b1b83195fc1d32d42bef353b751572aae8b629a
1808097 <1808097@rgu.ac.uk>
2022-04-10 22:53:16 +0100

    commenting out code

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -5,51 +5,51 @@ import config from './../../config/config'
 
 const signin = async (req, res) => {
   try {
-      let user = await User.findOne({
-        "email": req.body.email
-      })
-      if (!user)
-        return res.status('401').json({
-          error: "User not found"
-        })
-
-      if (!user.authenticate(req.body.password)) {
-        return res.status('401').send({
-          error: "Email and password don't match."
-        })
-      }
-
-      const token = jwt.sign({
-        _id: user._id
-      }, config.jwtSecret)
-
-      res.cookie("t", token, {
-        expire: new Date() + 9999
-      })
-
-      return res.json({
-        token,
-        user: {
-          _id: user._id,
-          name: user.name,
-          email: user.email
-        }
-      })
-
-    } catch (err) {
-
+    let user = await User.findOne({
+      "email": req.body.email
+    })
+    if (!user)
       return res.status('401').json({
-        error: "Could not sign in"
+        error: "User not found"
       })
 
+    if (!user.authenticate(req.body.password)) {
+      return res.status('401').send({
+        error: "Email and password don't match."
+      })
     }
+
+    const token = jwt.sign({
+      _id: user._id
+    }, config.jwtSecret)
+
+    res.cookie("t", token, {
+      expire: new Date() + 9999
+    })
+
+    return res.json({
+      token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    })
+
+  } catch (err) {
+
+    return res.status('401').json({
+      error: "Could not sign in"
+    })
+
+  }
 }
 
 const signout = (req, res) => {
   res.clearCookie("t")
-    return res.status('200').json({
-      message: "signed out"
-    })
+  return res.status('200').json({
+    message: "signed out"
+  })
 }
 
 const requireSignin = expressJwt({
@@ -60,6 +60,7 @@ const requireSignin = expressJwt({
 
 const hasAuthorization = (req, res, next) => {
 
+  /*
   console.log("TESTING1")
   console.log(req.profile.name)
   console.log("TESTING2")
@@ -67,7 +68,7 @@ const hasAuthorization = (req, res, next) => {
   console.log("TESTING3")
   console.log(req.profile._id)
   console.log("TESTING4")
-  console.log(req.auth._id)
+  console.log(req.auth._id)*/
 
   const authorized = req.profile && req.auth && req.profile._id == req.auth._id
   if (!(authorized)) {
