1f6dc265057e49822e9a5e56c01202989789baa5
1808097 <1808097@rgu.ac.uk>
2022-04-11 11:57:49 +0100

    forgot further methods

--- client/comment/AddComment.js
+++ client/comment/AddComment.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import IconButton from '@material-ui/core/IconButton'
 import Button from '@material-ui/core/Button'
@@ -10,89 +10,99 @@ import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import auth from './../auth/auth-helper'
 import { makeStyles } from '@material-ui/core/styles'
-import {create} from './api-comment.js'
-import {Redirect} from 'react-router-dom'
+import { create } from './api-comment.js'
+import { Redirect } from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
     card: {
-      maxWidth: 600,
-      margin: 'auto',
-      textAlign: 'center',
-      marginTop: theme.spacing(5),
-      paddingBottom: theme.spacing(2)
+        maxWidth: 600,
+        margin: 'auto',
+        textAlign: 'center',
+        marginTop: theme.spacing(5),
+        paddingBottom: theme.spacing(2)
     },
     error: {
-      verticalAlign: 'middle'
+        verticalAlign: 'middle'
     },
     title: {
-      marginTop: theme.spacing(2),
-      color: theme.palette.openTitle
+        marginTop: theme.spacing(2),
+        color: theme.palette.openTitle
     },
     textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      width: 300
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: 300
     },
     submit: {
-      margin: 'auto',
-      marginBottom: theme.spacing(2)
+        margin: 'auto',
+        marginBottom: theme.spacing(2)
     }
-  }))
+}))
 
 export default function AddComment() {
+    const [open, setOpen] = useState(false)
     const classes = useStyles()
-    const [values, setValues] = useState({
-      error: '',
-      originId: null,
-      user: '',
-      comment: '',
-    })
-  
-    const handleChange = name => event => {
-      setValues({ ...values, [name]: event.target.value })
+
+    const clickButton = () => {
+        setOpen(true)
     }
-  
+
+    const handleRequestClose = () => {
+        setOpen(false)
+    }
+
+    const [values, setValues] = useState({
+        error: '',
+        originId: null,
+        user: '',
+        comment: '',
+    })
+
+    const handleChange = name => event => {
+        setValues({ ...values, [name]: event.target.value })
+    }
+
     const clickSubmit = () => {
-      const comment = {
-        originId: auth.isAuthenticated._id || undefined,
-        user: values.email || undefined,
-        comment: values.comment || undefined
-      }
-      create(comment).then((data) => {
-        if (data.error) {
-          setValues({ ...values, error: data.error})
-        } else {
-          setValues({ ...values, error: '', open: true})
+        const comment = {
+            originId: auth.isAuthenticated._id || undefined,
+            user: values.email || undefined,
+            comment: values.comment || undefined
         }
-      })
+        create(comment).then((data) => {
+            if (data.error) {
+                setValues({ ...values, error: data.error })
+            } else {
+                setValues({ ...values, error: '', open: true })
+            }
+        })
     }
 
     return (<span>
-      <IconButton aria-label="Add Comment" onClick={clickButton} color="secondary">
-        <AddCommentIcon/>
-      </IconButton>
+        <IconButton aria-label="Add Comment" onClick={clickButton} color="secondary">
+            <AddCommentIcon />
+        </IconButton>
 
-      <Dialog open={open} onClose={handleRequestClose}>
-        <DialogTitle>{"Write Comment"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Confirm to delete the comment.
-          </DialogContentText>
-        </DialogContent>
-        <TextField id="comment" label="Comment" className={classes.textField} value={values.comment} onChange={handleChange('comment')} margin="normal"/><br/>
-        <DialogActions>
-          <Button onClick={handleRequestClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={clickSubmit} color="primary" autoFocus="autoFocus" variant="contained" className={classes.submit}>
-            Submit
-          </Button>
-        </DialogActions>
-      </Dialog>
+        <Dialog open={open} onClose={handleRequestClose}>
+            <DialogTitle>{"Write Comment"}</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    Confirm to delete the comment.
+                </DialogContentText>
+            </DialogContent>
+            <TextField id="comment" label="Comment" className={classes.textField} value={values.comment} onChange={handleChange('comment')} margin="normal" /><br />
+            <DialogActions>
+                <Button onClick={handleRequestClose} color="primary">
+                    Cancel
+                </Button>
+                <Button onClick={clickSubmit} color="primary" autoFocus="autoFocus" variant="contained" className={classes.submit}>
+                    Submit
+                </Button>
+            </DialogActions>
+        </Dialog>
     </span>)
 
 }
 AddComment.propTypes = {
-  commentId: PropTypes.string.isRequired
+    commentId: PropTypes.string.isRequired
 }
 
